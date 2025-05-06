cc8c9615cfc59c625e78db33eb52273d9b68ab3a
BlackLove4u <blacklovemovement4u@gmail.com>
2025-05-06 18:27:52 +0000

    Initial commit

---
+++ client/public/src/components/blog.js
@@ -0,0 +1,12 @@
+import React from 'react';  
+
+const Blog = () => {  
+    return (  
+        <div>  
+            <h1>Blog Page</h1>  
+            {/* Blog posts will be fetched and displayed here */}  
+        </div>  
+    );  
+};  
+
+export default Blog;  
---
+++ client/public/src/components/pages/index.js
@@ -0,0 +1,17 @@
+// filepath: /workspaces/nonprofit/public/index.html
+const htmlContent = `
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Nonprofit API</title>
+</head>
+<body>
+    <h1>Welcome to the Phoenix Rise Hub!</h1>
+    <p>This is the homepage for the Nonprofit API.</p>
+</body>
+</html>
+`;
+
+web: node client/server/models/controllers/server.js
