9df3096f3910a207e893da475612e91951af1c77
BlackLove4u <blacklovemovement4u@gmail.com>
2025-05-06 18:57:43 +0000

    updated

--- client/public/src/components/pages/index.js
+++ client/public/src/components/pages/index.js
@@ -1,4 +1,11 @@
 // filepath: /workspaces/nonprofit/public/index.html
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {  
+    console.log(`Server is running on http://localhost:${PORT}`);  
+});
+
+// filepath: /workspaces/nonprofit/public/src/components/pages/index.js
 const htmlContent = `
 <!DOCTYPE html>
 <html lang="en">
@@ -14,4 +21,12 @@ const htmlContent = `
 </html>
 `;
 
-web: node client/server/models/controllers/server.js
+module.exports = htmlContent;
+
+// filepath: /workspaces/nonprofit/client/server/models/controllers/server.js
+const htmlContent = require('../../../../public/src/components/pages/index');
+
+// Root route
+app.get('/', (req, res) => {
+    res.send(htmlContent); // Send the updated HTML content
+});
